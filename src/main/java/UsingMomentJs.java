28fe9d5a44e53dbb9054a6c48ab790d3a3c4313b
Shiva Saxena <shivasaxena@outlook.com>
2016-04-08 00:49:18 +0530

    added moment.js examples

---
+++ src/main/java/UsingMomentJs.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
+public class UsingMomentJs {
+
+	public static void main(String[] args) {
+		ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+		
+		try(InputStream is = HelloWorldFromJsFile.class.getResourceAsStream("HelloWorld.js")){
+		
+			BufferedReader br = new BufferedReader(new InputStreamReader(UsingMomentJs.class.getResourceAsStream("moment-with-locales.js")));
+			engine.eval(br);
+			
+			// date formatting
+			String convertedDate = engine.eval( "new moment(" +System.currentTimeMillis() + ").format('MMMM Do, YYYY');" ).toString();
+			System.out.println(convertedDate);
+			
+			convertedDate = engine.eval( "new moment(" +System.currentTimeMillis() + ").format('dddd, MMMM Do YYYY, h:mm:ss a');" ).toString();
+			System.out.println(convertedDate);
+			
+			
+			// date addition
+			convertedDate = engine.eval( "new moment().add(1, 'days').calendar();" ).toString();
+			System.out.println(convertedDate);
+			
+			// date internationalization (converting date to Hindi in this case)formatting
+			convertedDate = engine.eval( "new moment(" +System.currentTimeMillis() + ").locale('hi').format('MMMM Do, YYYY');" ).toString();
+			System.out.println(convertedDate);
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+}
+
+}
